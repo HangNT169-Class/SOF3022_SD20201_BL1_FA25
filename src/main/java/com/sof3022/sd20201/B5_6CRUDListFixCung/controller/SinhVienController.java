93d261a2e4a0373d76f80f573cd51c488878f514
HangNT169 <hangnt169@fpt.edu.vn>
2025-09-29 13:22:08 +0700

    Buổi 7: Spring Validation

--- src/main/java/com/sof3022/sd20201/B5_6CRUDListFixCung/controller/SinhVienController.java
+++ src/main/java/com/sof3022/sd20201/B5_6CRUDListFixCung/controller/SinhVienController.java
@@ -2,19 +2,26 @@ package com.sof3022.sd20201.B5_6CRUDListFixCung.controller;
 
 import com.sof3022.sd20201.B5_6CRUDListFixCung.entity.SinhVien;
 import com.sof3022.sd20201.B5_6CRUDListFixCung.service.SinhVienService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("sinh-vien/")
 public class SinhVienController {
 
     @Autowired
     private SinhVienService sinhVienService;
 
-    @GetMapping("sinh-vien/hien-thi")
+    @GetMapping("hien-thi")
+    // /sinh-vien/sinh-vien/hien-thi
     public String hienThiDanhSachSinhVien(Model m){
         // day list tu controller => html
         m.addAttribute("list1",sinhVienService.getList1());
@@ -22,24 +29,40 @@ public class SinhVienController {
     }
 
     // @Pathvariable
-    @GetMapping("sinh-vien/delete/{ma1}")
+    @GetMapping("delete/{ma1}")
     public String deleteSinhVien(@PathVariable("ma1") String ma){
         sinhVienService.xoaSinhVien(ma);
         return "redirect:/sinh-vien/hien-thi";
     }
 
-    @GetMapping("sinh-vien/detail/{ma}")
+    @GetMapping("detail/{ma}")
     public String detailSinhVien(@PathVariable String ma, Model m){
         SinhVien sv = sinhVienService.detailSinhVien(ma);
         m.addAttribute("sv1",sv);
         return "buoi4/detail-sinh-vien";
     }
 
-    @GetMapping("sinh-vien/view-update/{ma}")
+    @GetMapping("view-update/{ma}")
     public String viewUpdateSinhVien(@PathVariable String ma, Model m){
         SinhVien sv = sinhVienService.detailSinhVien(ma);
         m.addAttribute("sv1",sv);
         return "buoi4/update-sinh-vien";
+    }
+
+    @GetMapping("view-add")
+    public String viewAddSinhVien(Model m){
+        m.addAttribute("sv2",new SinhVien());
+        return "buoi4/add-sinh-vien";
+    }
+
+    @PostMapping("add")
+    public String addSinhVien(@Valid @ModelAttribute("sv2") SinhVien sv,
+                              BindingResult result){
+        if(result.hasErrors()){
+            return "buoi4/add-sinh-vien";
+        }
+        sinhVienService.add(sv);
+        return "redirect:/sinh-vien/hien-thi";
     }
 
 }
--- src/main/java/com/sof3022/sd20201/B5_6CRUDListFixCung/entity/SinhVien.java
+++ src/main/java/com/sof3022/sd20201/B5_6CRUDListFixCung/entity/SinhVien.java
@@ -1,5 +1,12 @@
 package com.sof3022.sd20201.B5_6CRUDListFixCung.entity;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,15 +20,26 @@ public class SinhVien {
 //    Tạo đối tượng SinhVien gồm các thuộc tính: mssv - String, ten-String,
 //    tuoi- int, diaChi - String, gioiTinh - boolean và các constructor,
 //    getter, setter
-
+    // cac loai kieu du lieuj
+    // String => @NotBlank/NotEmpty
+    // Khac => @NotNull
+    @NotBlank
+//    @Pattern(regexp = "\\d+",message = "MSSV k dung dinh dang")
+//    @Size(min = 5,message = "Vuot qua khoang cho phep") // do dai cua chuoi
     private String mssv;
 
+    @NotEmpty(message = "Ten khong duoc de trong")
     private String ten;
 
+    // https://viblo.asia/p/dung-validation-bang-tay-trong-spring-boot-phan-1-3P0lPGyoZox
+    @NotNull(message = "Tuoi khong duoc de trong")
+//    @Min(value = 18,message = "Tuoi ngoai khoang cho phep")
+//    @Max(value = 35,message = "Tuoi ngoai khoang cho phep")
     private Integer tuoi;
 
     private String diaChi;
 
+    @NotNull(message = "Gioi tinh khong duoc de trong")
     private Boolean gioiTinh;
 
 }
